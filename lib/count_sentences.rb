65941027152c0e0f5f9097138b6d29a03592ed92
Daniel Brazel <dbrazel4@gmail.com>
2018-04-06 14:38:20 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.split("")[-1] == "."
   end
 
   def question?
-
+    self.split("")[-1] == "?"
   end
 
   def exclamation?
-
+    self.split("")[-1] == "!"
   end
 
   def count_sentences
+    self.split(".").map{|el|el.split("!")}.flatten.map{|el| el.split("?")}.length
+    # binding.pry
 
   end
 end
